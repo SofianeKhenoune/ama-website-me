ac881ce27fb6ccd21edf053c12a9271212b1492a
Sofiane <modestman1986@yahoo.com>
2023-12-16 17:14:48 +0100

    feat: test for navBar ok

--- app/ui/nav-links.tsx
+++ app/ui/nav-links.tsx
@@ -2,15 +2,16 @@
 import clsx from "clsx"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import { FaMosque } from "react-icons/fa"
 import {
   HiHome,
   HiMiniInformationCircle,
   HiOutlineNewspaper,
 } from "react-icons/hi2"
 import { MdContactMail } from "react-icons/md"
+import useSetMenu from "../store/menuStore"
 
 // Map of links to display in the side navigation.
+
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: "Accueil", href: "/", icon: HiHome },
@@ -20,17 +21,13 @@ const links = [
     href: "/news",
     icon: HiOutlineNewspaper,
   },
-  {
-    name: "Horaires de prières",
-    href: "/prayer",
-    icon: FaMosque,
-  },
+
   { name: "Contact", href: "/contact", icon: MdContactMail },
 ]
 
 export default function NavLinks() {
   const pathname = usePathname()
-
+  const { setMenu } = useSetMenu()
   return (
     <div className={"navlinks flex flex-col space-x-0 w-full min-h-full gap-2"}>
       {links.map((link) => {
@@ -45,9 +42,10 @@ export default function NavLinks() {
                 "bg-light text-primary": pathname === link.href,
               }
             )}
+            onClick={() => setMenu()}
           >
             <LinkIcon className="w-6" />
-            <p className="">{link.name}</p>
+            <p>{link.name}</p>
           </Link>
         )
       })}
--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -2,10 +2,16 @@
 import NavLinks from "@/app/ui/nav-links"
 import Link from "next/link"
 import { CgCloseO } from "react-icons/cg"
-
+import useSetMenu from "../store/menuStore"
 export default function SideNav() {
+  const { open, setMenu } = useSetMenu()
+
   return (
-    <div className="sidenav flex h-screen flex-col px-3 py-4 md:px-3 bg-primary justify-between">
+    <div
+      className={`sidenav h-screen px-3 py-4 md:px-3 bg-sideNavColor fixed w-full flex flex-col md:relative transition-transform duration-500 md:translate-x-0 ${
+        open ? "translate-x-0" : "-translate-x-full"
+      }`}
+    >
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md p-4 md:h-40"
         href="/"
@@ -13,12 +19,15 @@ export default function SideNav() {
           background: 'center / contain url("./images/logo.png") no-repeat',
         }}
       ></Link>
+
       <div className="flex justify-between flex-col md:grow">
         <NavLinks />
       </div>
       <CgCloseO
-        className="self-end md:hidden hover:scale-110 cursor-pointer"
-        size={30}
+        className="fixed md:hidden hover:scale-110 cursor-pointer bottom-5 right-5 bg-medium rounded-md p-1"
+        size={50}
+        color="white"
+        onClick={() => setMenu()}
       />
     </div>
   )
