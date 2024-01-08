0eefee542732436c6948b2f2c509dfb2c4f1a3c9
Sofiane <modestman1986@yahoo.com>
2024-01-09 00:15:19 +0100

    style: add animation to homepage

--- app/about/history/page.tsx
+++ app/about/history/page.tsx
@@ -1,12 +1,30 @@
+"use client"
 import { AmaHistory } from "@/app/data/history"
 import HistoryComponent from "@/app/ui/historyComponent"
 import clsx from "clsx"
+import gsap from "gsap"
+import { useEffect } from "react"
 
 const History = () => {
   const oddElemClassnames =
-    "rounded-tl-xl rounded-bl-xl border-medium border-t-8 border-l-8 -mb-2 -mt-2 -ml-2 relative pb-8 px-6"
+    "rounded-tl-xl rounded-bl-xl border-t-8 border-l-8 -mb-2 -mt-2 -ml-2 relative pb-8 px-6 opacity-0 -translate-x-full"
   const evenElemClassnames =
-    "rounded-tr-xl rounded-br-xl border-medium border-r-8 border-t-8 -mr-2 relative pb-8 px-6"
+    "rounded-tr-xl rounded-br-xl border-r-8 border-t-8 -mr-2 relative pb-8 px-6 opacity-0 translate-x-full"
+  useEffect(() => {
+    const tl = gsap.timeline({ delay: 1 })
+    const blocs = gsap.utils.toArray("section > *")
+    blocs.forEach((element: any) => {
+      tl.to(element, {
+        opacity: 1,
+        duration: 1,
+        x: 0,
+      })
+      tl.to(element, {
+        borderColor: "hsla(188, 76%, 31%,1)",
+        duration: 1,
+      })
+    })
+  })
   return (
     <main className="min-h-screen flex flex-col overflow-hidden">
       <h1 className="text-title1 font-bold text-center py-2 px-7 bg-light rounded-bl-full rounded-br-full">
--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,18 @@ import { useEffect } from "react"
 export default function Home() {
   useEffect(() => {
     const tl = gsap.timeline({ delay: 1 })
+    const blocs = gsap.utils.toArray(".homepage > *")
     tl.to(".homepage", {
       opacity: 1,
       duration: 2,
     })
-    tl.to(".homepage h1", {
-      opacity: 1,
-      y: 0,
-      duration: 2,
+    blocs.forEach((element: any) => {
+      tl.to(element, {
+        opacity: 1,
+        duration: 1,
+        y: 0,
+        x: 0,
+      })
     })
   })
   return (
@@ -20,16 +24,21 @@ export default function Home() {
         Bienvenue !
       </h1>
       <div
-        className="homepage flex-1 opacity-0 flex justify-center items-center p-5"
+        className="homepage flex-1 opacity-0 flex flex-col justify-evenly gap-5 p-5"
         style={{
           background:
             "center / contain no-repeat url(/images/background-mosq.svg)",
         }}
       >
-        <h1 className="text-title1 font-bold py-2 px-7 opacity-0 -translate-y-full bg-dark text-slate-50 max-w-max rounded-lg text-center">
-          Aux côtés des Musulmans d&apos;Aubervilliers depuis 2001
+        <h1 className="text-title1 font-bold py-2 px-7 opacity-0 -translate-y-full -translate-x-full bg-dark text-slate-50 max-w-max rounded-lg text-center self-baseline">
+          Association des Musulmans d&apos;Aubervilliers
         </h1>
-        <h2></h2>
+        <h2 className="text-title1 font-bold py-2 px-7 opacity-0 -translate-y-full -translate-x-full bg-dark text-slate-50 max-w-max rounded-lg text-center self-center">
+          Aux côtés des Musulmans d&apos;Aubervilliers depuis 2001
+        </h2>
+        <p className="text-title1 font-bold py-2 px-7 opacity-0 -translate-y-full -translate-x-full bg-dark text-slate-50 max-w-max rounded-lg text-center self-end">
+          Acquisition de 2 lieux de cultes
+        </p>
       </div>
     </main>
   )
